0a8da44b43f5959d80b092ad877e76d2b92714bb
Albert Litewka <albertlitewka@wp.pl>
2022-02-18 18:23:52 +0100

    Creating more tests

--- Factorial_Fibonacci_Tribonacci-Numbers/src/test/java/FunctionsTest.java
+++ Factorial_Fibonacci_Tribonacci-Numbers/src/test/java/FunctionsTest.java
@@ -44,10 +44,8 @@ public class FunctionsTest {
 		
 //		given
 		
-		int number = 5;
-		
-		int number2 = 12;
-		
+		int number = 5;		
+		int number2 = 12;		
 		int number3 = 2;
 		
 //		then
@@ -62,10 +60,8 @@ public class FunctionsTest {
 		
 //		given
 		
-		int number = 13;
-		
-		int number2 = 6;
-		
+		int number = 13;		
+		int number2 = 6;		
 		int number3 = 0;
 		
 //		then
@@ -75,8 +71,102 @@ public class FunctionsTest {
 		Assert.assertNotEquals(Functions.fibonacci(number3), 12);
 	}
 	
+	@Test
+	public void should_calculate_fibonacciRecursion() {
+		
+//		given
+		
+		int number = 1;		
+		int number2 = 8;		
+		int number3 = 11;
+		
+//		then
+		
+		Assert.assertEquals(Functions.fibonacciRecursion(number), 1);
+		Assert.assertEquals(Functions.fibonacciRecursion(number2), 21);
+		Assert.assertEquals(Functions.fibonacciRecursion(number3), 89);
+	}
+	
+	@Test
+	public void should_not_calculate_fibonacciRecursion() {
+		
+//		given
+		
+		int number = 3;		
+		int number2 = 1;		
+		int number3 = 9;
+		
+//		then
+		
+		Assert.assertNotEquals(Functions.fibonacciRecursion(number), 25);
+		Assert.assertNotEquals(Functions.fibonacciRecursion(number2), 7);
+		Assert.assertNotEquals(Functions.fibonacciRecursion(number3), 121);
+	}
 	
 	
+	@Test
+	public void should_calculate_tribonacci() {
+		
+//		given
+		
+		int number = 10;	
+		int number2 = 7;
+		int number3 = 3;
+		
+//		then
+		
+		Assert.assertEquals(Functions.tribonacci(number), 44);
+		Assert.assertEquals(Functions.tribonacci(number2), 7);
+		Assert.assertEquals(Functions.tribonacci(number3), 1);
+	}
+	
+	@Test
+	public void should_not_calculate_tribonacci() {
+		
+//		given
+		
+		int number = 13;		
+		int number2 = 6;		
+		int number3 = 0;
+		
+//		then
+		
+		Assert.assertNotEquals(Functions.tribonacci(number), 1225);
+		Assert.assertNotEquals(Functions.tribonacci(number2), 127);
+		Assert.assertNotEquals(Functions.tribonacci(number3), 5);
+	}
+	
+	@Test
+	public void should_calculate_tribonacciRecursion() {
+		
+//		given
+		
+		int number = 1;	
+		int number2 = 12;
+		int number3 = 7;
+		
+//		then
+		
+		Assert.assertEquals(Functions.tribonacciRecursion(number), 0);
+		Assert.assertEquals(Functions.tribonacciRecursion(number2), 149);
+		Assert.assertEquals(Functions.tribonacciRecursion(number3), 7);
+	}
+	
+	@Test
+	public void should_not_calculate_tribonacciRecursion() {
+		
+//		given
+		
+		int number = 4;		
+		int number2 = 2;		
+		int number3 = 9;
+		
+//		then
+		
+		Assert.assertNotEquals(Functions.tribonacciRecursion(number), 125);
+		Assert.assertNotEquals(Functions.tribonacciRecursion(number2), 27);
+		Assert.assertNotEquals(Functions.tribonacciRecursion(number3), 1125);
+	}
 	
 	
 	
